8625532d873631d60d0fb27be2895ad32d11bf2f
Adam Beck <AdamB.Infinity@gmail.com>
2016-02-25 22:59:16 -0500

    Fix variable for logging in

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+# Make sure to not use any print statements as the node server
+# is communicating with this script based on the output of prints
+#
+# It might make sense to implement a messaging service that will handle
+# the communication in a much cleaner manner
+
 import re
 import os
 from gmusicapi import Mobileclient
@@ -6,7 +12,7 @@ username = os.environ.get( 'GMUSIC_USERNAME' )
 password = os.environ.get( 'GMUSIC_PASSWORD' )
 
 api = Mobileclient()
-api.login(GMUSIC_USERNAME, GMUSIC_PASSWORD, Mobileclient.FROM_MAC_ADDRESS)
+logged_in = api.login(username, password, Mobileclient.FROM_MAC_ADDRESS)
 
 tracks = api.get_station_tracks('44b0d91f-1696-303b-ade0-3f67767a47da', 1)
 
